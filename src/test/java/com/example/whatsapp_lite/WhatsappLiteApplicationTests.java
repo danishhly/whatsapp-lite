31e73616e43e7e1ab9aea9a366a0fa35d092e433
Daan153 <creative.in47@gmail.com>
2025-11-20 05:40:59 +0530

    saving my changes

---
+++ src/test/java/com/example/whatsapp_lite/WhatsappLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsapp_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

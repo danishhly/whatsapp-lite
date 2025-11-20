ac0fdddc7889507a3ddbe7a39d7889726a2a7f26
Daan153 <creative.in47@gmail.com>
2025-11-20 07:11:52 +0530

    Add ChatMessageRepository with query to fetch recent messages

--- src/main/java/com/example/whatsapp_lite/dto/MessagePayload.java
+++ src/main/java/com/example/whatsapp_lite/dto/MessagePayload.java
@@ -1,9 +1,13 @@
 package com.example.whatsapp_lite.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessagePayload {
     private String senderId;
     private String receiverId;
---
+++ src/main/java/com/example/whatsapp_lite/repo/ChatMessageRepository.java
@@ -0,0 +1,10 @@
+package com.example.whatsapp_lite.repo;
+
+import com.example.whatsapp_lite.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findTop100ByChatIdOrderByTimeStampDesc(String chatId);
+}
